234f0b9bccf1f169c0940ca47ac84a600eea12dd
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-05-07 18:49:30 +0530

    Create uploadMiddleware.js

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = "uploads/blogs";
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
