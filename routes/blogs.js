7aa0b39f58873cdb348d6a54fd3e224c05aa4c8f
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-05-13 16:07:43 +0530

    Refactor blog routes to include stats and like toggle

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,33 +1,22 @@
 const express = require("express");
-
 const router = express.Router();
-
 const uploadBlog = require("../middleware/uploadBlog");
-
 const {
   getBlogs,
   getBlogBySlug,
   createBlog,
   updateBlog,
   deleteBlog,
+  getBlogStats,
+  toggleLike,
 } = require("../controllers/blogController");
 
 router.get("/", getBlogs);
-
+router.get("/stats", getBlogStats);
 router.get("/:slug", getBlogBySlug);
-
-router.post(
-  "/",
-  uploadBlog.single("coverImage"),
-  createBlog
-);
-
-router.put(
-  "/:id",
-  uploadBlog.single("coverImage"),
-  updateBlog
-);
-
+router.post("/", uploadBlog.single("coverImage"), createBlog);
+router.put("/:id", uploadBlog.single("coverImage"), updateBlog);
 router.delete("/:id", deleteBlog);
+router.post("/:id/like", toggleLike);
 
 module.exports = router;
