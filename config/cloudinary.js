aec2da45699b29575c76ca7c44acdc96791181b1
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-08 16:09:56 +0530

    Refactor cloudinary.js to simplify exports
    Removed CloudinaryStorage configuration and exported only the cloudinary instance.

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,27 @@
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'blogs',
+//     allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
+//     transformation: [{ width: 1200, height: 800, crop: 'limit' }],
+//   },
+// });
+
+// module.exports = { cloudinary, storage };
+
+
+
+
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,13 +29,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'blogs',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
-    transformation: [{ width: 1200, height: 800, crop: 'limit' }],
-  },
-});
-
-module.exports = { cloudinary, storage };
+module.exports = cloudinary;
