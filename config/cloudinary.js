065d2a5fb11436e6ceddc9904ee11f69e3c18800
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-08 18:04:49 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -21,18 +21,7 @@
 
 // 2nd
 
-// const cloudinary = require('cloudinary').v2;
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// module.exports = cloudinary;
-
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -40,15 +29,26 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'blogs',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
-    transformation: [{ width: 1200, height: 800, crop: 'limit' }],
-  },
-});
+module.exports = cloudinary;
 
-module.exports = { cloudinary, storage };
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'blogs',
+//     allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
+//     transformation: [{ width: 1200, height: 800, crop: 'limit' }],
+//   },
+// });
+
+// module.exports = { cloudinary, storage };
 
 
