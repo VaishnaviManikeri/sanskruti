52feebaebbb0efba24cfa1ec62507484ad0642b9
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-08 16:20:36 +0530

    Add authentication middleware to admin routes

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const uploadBlog = require("../middleware/uploadBlog");
+const authMiddleware = require("../middleware/auth");
 const {
   getBlogs,
   getBlogBySlug,
@@ -9,14 +10,20 @@ const {
   deleteBlog,
   getBlogStats,
   toggleLike,
+  getAllBlogsAdmin,
 } = require("../controllers/blogController");
 
+// Public routes
 router.get("/", getBlogs);
 router.get("/stats", getBlogStats);
 router.get("/:slug", getBlogBySlug);
+router.post("/:id/like", toggleLike);
+
+// Admin routes (protected)
+router.use(authMiddleware);
+router.get("/admin/all", getAllBlogsAdmin);
 router.post("/", uploadBlog.single("coverImage"), createBlog);
 router.put("/:id", uploadBlog.single("coverImage"), updateBlog);
 router.delete("/:id", deleteBlog);
-router.post("/:id/like", toggleLike);
 
 module.exports = router;
