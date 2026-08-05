31cd3bec21addca1dbd1909491b4beb0436948f9
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-08-05 12:34:16 +0530

    Add generateThumbnail method to Video model
    Added a method to generate thumbnails from YouTube URLs.

--- models/Video.js
+++ models/Video.js
@@ -44,4 +44,21 @@ const videoSchema = new mongoose.Schema(
   }
 );
 
+// Generate thumbnail from YouTube URL or Vimeo URL
+videoSchema.methods.generateThumbnail = function() {
+  if (!this.videoUrl) return null;
+  
+  // YouTube thumbnail
+  const youtubeRegex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i;
+  const youtubeMatch = this.videoUrl.match(youtubeRegex);
+  
+  if (youtubeMatch) {
+    return `https://img.youtube.com/vi/${youtubeMatch[1]}/maxresdefault.jpg`;
+  }
+  
+  // Vimeo thumbnail (requires API call, we'll handle in controller)
+  // For other URLs, return default thumbnail
+  return null;
+};
+
 module.exports = mongoose.model('Video', videoSchema);
